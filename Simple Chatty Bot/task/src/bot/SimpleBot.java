f66122b8293bf5c73612d0d1e0d783ac0c503425
Zaid Neurothrone <zaid_neurothrone@hotmail.com>
2021-01-19 01:24:09 +0100

    Complete Stage 3/5: Guess the age

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -6,12 +6,23 @@ public class SimpleBot {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello! My name is Zin.");
-        System.out.println("I was created in 2021.");
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
         System.out.println("Please, remind me your name.");
 
         String userName = scanner.nextLine();
 
-        System.out.format("What a great name you have, %s!", userName);
+        System.out.println("What a great name you have, " + userName + "!");
+
+
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int userAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.format("Your age is %d; that's a good time to start programming!", userAge);
     }
 }
